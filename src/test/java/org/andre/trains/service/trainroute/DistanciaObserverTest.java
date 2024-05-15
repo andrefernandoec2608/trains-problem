83692a05b74729be38be5aeb09228f8920f521df
andrefernandoec2608 <andre.thinking@gmail.com>
2024-05-16 00:41:35 +0800

    Inicio

--- src/test/java/org/andre/trains/service/trainroute/DistanciaObserverTest.java
+++ src/test/java/org/andre/trains/service/trainroute/DistanciaObserverTest.java
@@ -1,9 +1,32 @@
 package org.andre.trains.service.trainroute;
 
 import junit.framework.TestCase;
+import org.andre.trains.service.load.ManejadorCarga;
+import org.andre.trains.source.grafo.GrafoBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class DistanciaObserverTest extends TestCase {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    ManejadorCarga manejadorCarga = ManejadorCarga.getInstance();
+    Observer disObserver = new DistanciaObserver(GrafoBuilder.getDefaultGraph(manejadorCarga), manejadorCarga.getIndicacionesListado());
+
+    protected void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    protected void tearDown() {
+        System.setOut(originalOut);
+    }
     public void testProcesar() {
+        disObserver.procesar();
+        assertTrue("Respuesta: 9", outContent.toString().contains("Respuesta: 9"));
+        assertTrue("Respuesta: 5", outContent.toString().contains("Respuesta: 5"));
+        assertTrue("Respuesta: 13", outContent.toString().contains("Respuesta: 13"));
+        assertTrue("Respuesta: 22", outContent.toString().contains("Respuesta: 22"));
     }
 }
